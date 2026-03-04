80381512decfe551ef2fdf62d9731a953b0e5311
Perrin Emmanuel Nzaou <perrinemmanuelnzaou@gmail.com>
2026-03-04 14:22:41 +0000

    définition de nos constantes de couleurs, création du hook qui nre retourne des couleurs selon le theme du téléphone (dark ou light)

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,6 +1,38 @@
 export const Colors = {
-    light: {
-        tint: "#DC"
-    },
-    dark: {}
-}
+  light: {
+    tint: "#DC0A2D",
+    grayDark: "#212121",
+    grayMedium: "#666666",
+    grayLight: "#E0E0E0",
+    grayBackground: "#EFEFEF",
+    grayWhite: "#FFFFFF",
+  },
+  dark: {
+    tint: "#DC0A2D",
+    grayDark: "#212121",
+    grayMedium: "#666666",
+    grayLight: "#E0E0E0",
+    grayBackground: "#EFEFEF",
+    grayWhite: "#FFFFFF",
+  },
+  type: {
+    Bug: "#A7B723",
+    Dark: "#75574C",
+    Dragon: "#7037FF",
+    Electric: "#F9CF30",
+    Fairy: "#E69EAC",
+    Fighting: "#C12239",
+    Fire: "#F57D31",
+    Flying: "#A891EC",
+    Ghost: "#70559B",
+    Normal: "#AAA67F",
+    Grass: "#74CB48",
+    Ground: "#DEC16B",
+    Ice: "#9AD6DF",
+    Poison: "#A43E9E",
+    Psychic: "#FB5584",
+    Rock: "#B69E31",
+    Steel: "#B7B9D0",
+    Water: "#6493EB",
+  },
+};
